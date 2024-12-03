4edc487431d52010cb3f09d115304106c354c93c
josisantos12 <josiane15santos24@gmail.com>
2024-12-03 16:51:16 -0300

    atualizado Hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+import { HeroStyled, TitleStyled } from './styles';
+
+
+const Hero = () => (
+  <HeroStyled>
+    <div className="container">
+      <TitleStyled>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </TitleStyled>
+    </div>
+  </HeroStyled>
+)
+
+export default Hero
